880ebf63a9c10ad4d35503a14de82c6d77ddbdc3
JohnMarston-cooking <csazid79@gmail.com>
2025-08-14 10:08:00 +0600

    Updated post-chroot.sh

--- post-chroot.sh
+++ post-chroot.sh
@@ -1,4 +1,3 @@
 echo -e "deb https://mirror.xeonbd.com/debian trixie main contrib non-free non-free-firmware\ndeb https://mirror.xeonbd.com/debian trixie-updates main contrib non-free non-free-firmware\ndeb http://security.debian.org trixie-security main contrib non-free non-free-firmware\ndeb https://mirror.xeonbd.com/debian trixie-backports main contrib non-free non-free-firmware" >> etc/apt/sources.list
 apt update
-apt install broadcom-sta-dkms linux-headers-6.12.38+deb13-amd64 -y
-exit
+apt install broadcom-sta-dkms linux-headers-6.12.38+deb13-amd64 network-manager -y
